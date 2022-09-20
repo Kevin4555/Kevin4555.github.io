f9071d942ab64083a26891cd7b8f21bdf415812d
KChino <kevintlapizaga@gmail.com>
2022-09-20 20:20:42 -0300

    implemente product-info
    ..

---
+++ js/product-info.js
@@ -0,0 +1,104 @@
+function Cambiarprod(id) {
+    localStorage.setItem("productoID",id);
+    window.location="product-info.html";
+}
+function Showdata(product) {
+    document.getElementById("tituloprod").innerHTML=product.name;
+    let currency=product.currency;
+    if (product.currency=="UYU") {
+        currency="$"
+    }    
+    //Agrego los datos
+    document.getElementById("precioprod").innerHTML=currency+product.cost;
+    document.getElementById("descprod").innerHTML=product.description;
+    document.getElementById("categprod").innerHTML=product.category;
+    document.getElementById("cantvendprod").innerHTML=product.soldCount;
+    //Agrego las imagenes al carousel
+    for (let index = 0; index < product.images.length; index++) {
+        if (index==0) {
+            document.getElementById("imagenescarousel").innerHTML=
+            ` 
+            <div class="carousel-item active">
+            <img src="${product.images[index]}" class="d-block w-100" alt="img${index+1}">
+            </div>
+            `
+        }else{
+            document.getElementById("imagenescarousel").innerHTML+=
+            ` 
+            <div class="carousel-item">
+            <img src="${product.images[index]}" class="d-block w-100" alt="img${index+1}">
+            </div>
+            `
+        }
+    }
+    //Agrego los productos relacionasdos
+    document.getElementById("ProdR").innerHTML="";
+    for (let element of product.relatedProducts) {
+    let pr=`
+    <div class="card " onclick=Cambiarprod(${element.id})>
+        <img src="${element.image}" class="card-img-top" alt="image-${element.name}">
+        <div class="card-body">
+            <h5 class="card-title">${element.name}</h5>
+        </div>
+    </div>
+        `
+        document.getElementById("ProdR").innerHTML+=pr;
+    }
+}
+function ShowComments(comments) {
+    document.getElementById("listcomments").innerHTML="";
+    for (let comment of comments) {
+        let text=`
+        <div class="list-group-item">
+        <p><strong>${comment.user}</strong> - ${comment.dateTime} - `;
+        for (let index = 0; index < 5; index++) {
+            if (index<comment.score) {
+            text+=`<span class="fa fa-star checked"></span>`;
+            }else
+            text+=`<span class="fa fa-star"></span>`; 
+        }
+        text+=`</p>
+        <p>${comment.description}</p>
+        </div>
+        `;
+        document.getElementById("listcomments").innerHTML+=text;
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    if (localStorage.getItem("productoID")) {
+        getJSONData(PRODUCT_INFO_URL+localStorage.getItem("productoID")+EXT_TYPE).then(function(resultObj){
+            if(resultObj.status === "ok"){
+                let producto=resultObj.data;
+                console.log(producto)
+                Showdata(producto);
+            }
+        }).then(function(){
+          getJSONData(PRODUCT_INFO_COMMENTS_URL+localStorage.getItem("productoID")+EXT_TYPE).then(function(resultObj2){
+            if (resultObj2.status==="ok") {
+                let commentsArray=resultObj2.data;
+                if (commentsArray.length==0) {
+                    document.getElementById("listcomments").innerHTML=`
+                    <div class="list-group-item text-center">
+                    <p>Aun no hay comentarios</p>
+                    </div>
+                    `;
+                }else{
+                  ShowComments(commentsArray);  
+                }
+            }
+        })
+        })
+        document.getElementById("btncomment").addEventListener("click",function(){
+            document.getElementById("textcomment").value="";
+            document.getElementById("scorecommet").selectedIndex=0;
+        })
+    }
+    else {
+        alert("No selecciono ningún Producto");
+        window.location="products.html";
+    }
+})
--- js/products.js
+++ js/products.js
@@ -2,13 +2,18 @@ let category;
 let min=undefined;
 let max=undefined;
 
+function Reditigirainfo(productoid) {
+    localStorage.setItem("productoID",productoid);
+    window.location="product-info.html";
+}
+
 function showProductsList(products) {
     document.getElementById("cat-list-container").innerHTML="";
     let text="";
     for (let product of products) {
         if ((!(parseInt(product.cost) < min)) && (!(parseInt(product.cost) > max))) {
         text=`
-            <div class="list-group-item ">
+            <div class="list-group-item list-group-item-action cursor-active" onclick=Reditigirainfo(${product.id})>
             <div class="row">
                 <div class="col-3">
                     <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
@@ -32,33 +37,37 @@ function showProductsList(products) {
 
 
 document.addEventListener("DOMContentLoaded",function () {
-    getJSONData(PRODUCTS_URL+localStorage.getItem("catID")+EXT_TYPE).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            category = resultObj.data;
-            document.getElementById("subtitle").innerHTML+= category.catName+".";
-            showProductsList(category.products);
-
-            document.getElementById("filtrarProd").addEventListener("click",function(){
-                min=parseInt(document.getElementById("filtrominP").value);
-                max=parseInt(document.getElementById("filtromaxP").value);
+    if (localStorage.getItem("catID")) {
+        getJSONData(PRODUCTS_URL+localStorage.getItem("catID")+EXT_TYPE).then(function(resultObj){
+            if (resultObj.status === "ok"){
+                category = resultObj.data;
+                document.getElementById("subtitle").innerHTML+= category.catName+".";
                 showProductsList(category.products);
-            })
-            document.getElementById("limpiarProd").addEventListener("click",function(){
-                document.getElementById("filtrominP").value="";
-                document.getElementById("filtromaxP").value="";
-                min=undefined;
-                max=undefined;
-                showProductsList(category.products);
-            })
-            document.getElementById("asdP").addEventListener("click",function () {
-                showProductsList(category.products.sort(function(a, b){return parseInt(a.cost)-parseInt(b.cost)}));
-            })
-            document.getElementById("desP").addEventListener("click",function () {
-                showProductsList(category.products.sort(function(a, b){return parseInt(b.cost)-parseInt(a.cost)}));
-            })
-            document.getElementById("desR").addEventListener("click",function () {
-                showProductsList(category.products.sort(function(a, b){return parseInt(b.soldCount)-parseInt(a.soldCount)}));
-            })
-        
-        };
-    })});
+    
+                document.getElementById("filtrarProd").addEventListener("click",function(){
+                    min=parseInt(document.getElementById("filtrominP").value);
+                    max=parseInt(document.getElementById("filtromaxP").value);
+                    showProductsList(category.products);
+                })
+                document.getElementById("limpiarProd").addEventListener("click",function(){
+                    document.getElementById("filtrominP").value="";
+                    document.getElementById("filtromaxP").value="";
+                    min=undefined;
+                    max=undefined;
+                    showProductsList(category.products);
+                })
+                document.getElementById("asdP").addEventListener("click",function () {
+                    showProductsList(category.products.sort(function(a, b){return parseInt(a.cost)-parseInt(b.cost)}));
+                })
+                document.getElementById("desP").addEventListener("click",function () {
+                    showProductsList(category.products.sort(function(a, b){return parseInt(b.cost)-parseInt(a.cost)}));
+                })
+                document.getElementById("desR").addEventListener("click",function () {
+                    showProductsList(category.products.sort(function(a, b){return parseInt(b.soldCount)-parseInt(a.soldCount)}));
+                })
+            };
+        })
+    } else {
+        window.location="categories.html";
+    }
+    });
