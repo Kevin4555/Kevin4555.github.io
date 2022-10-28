976cfd0911b27f4f163779274a580bbe04b367b6
KChino <kevintlapizaga@gmail.com>
2022-10-28 02:34:14 -0300

    Agrego Finalizar compra
    Agrego Finalizar compra y validaciones

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,59 @@
 let cart;
 let listaprodcart = document.getElementById("listacarrito");
 let subtotal = 0;
-let costenvio = {
-    tipo: "Premium",
-    porcentaje: 15
-};
+let costenvio = "No Seleccionado";
 let costodeenvio = 0;
 let mostrarenuru = false;
 let mediodepago;
-
+function actualizarfpago() {
+    if (document.querySelector("#listacarrito.was-validated")){
+    if (document.getElementById("formapg").value == "No ha Seleccionado") {
+        document.getElementById("formapgfeedback").innerHTML = "<small>Deberia seleccionar una forma de pago</small>";
+    } else {
+        if ((document.getElementById("num_tarj").value != "" && document.getElementById("codseg").value != "" && document.getElementById("venc").value != "") || (document.getElementById("num_cuenta").value != "")) {
+            document.getElementById("formapgfeedback").innerHTML = "";
+        } else {
+            document.getElementById("formapgfeedback").innerHTML = "Faltan datos al seleccionar una forma de pago";
+        }
+    }    
+    }
+}
 function actualizarcostos() {
-    costodeenvio = (costenvio.porcentaje * subtotal) / 100;
+
     let moneda = "USD";
     if (mostrarenuru) {
         moneda = "UYU";
     }
-    document.getElementById("costos").innerHTML = `
+    let html = `
     <div class="list-group-item">
     <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(subtotal)}</p>
     <p>Subtotal</p>
-    </div> 
-    <div class="list-group-item">
-    <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(costodeenvio)}</p>
-    <p>Envío: ${costenvio.tipo}</p>
-    </div>
-    <div class="list-group-item">
-    <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(costodeenvio + subtotal)}</p>
-    <p>Total</p>
-    </div> 
-    `;
+    </div> `
+    if (costenvio == "No Seleccionado") {
+        html += `
+        <div class="list-group-item">
+        <p class="float-end">######</p>
+        <p>Envío: ${costenvio}</p>
+        </div>
+        <div class="list-group-item">
+        <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(subtotal)}</p>
+        <p>Total</p>
+        </div> 
+        `;
+    } else {
+        costodeenvio = (costenvio.porcentaje * subtotal) / 100;
+        html += `
+        <div class="list-group-item">
+        <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(costodeenvio)}</p>
+        <p>Envío: ${costenvio.tipo}</p>
+        </div>
+        <div class="list-group-item">
+        <p class="float-end">${moneda} ${Intl.NumberFormat('es-UY').format(costodeenvio + subtotal)}</p>
+        <p>Total</p>
+        </div> 
+        `;
+    }
+    document.getElementById("costos").innerHTML = html;
 }
 
 function eliminarprod(id) {
@@ -71,8 +96,8 @@ function ShowProductCart(cart) {
         <td class="border-dark">${cart[prod].name}</td>
         <td class="border-dark">${cart[prod].currency}<br>${formatcost}</td>
         <td class="border-dark">
-            <input maxlength="3" type="number" name="${prod}" value=${cart[prod].cantidad} style="
-            width: 83px;">
+            <input maxlength="3" type="number" class="form-control" name="${prod}" value=${cart[prod].cantidad} style="
+            width: 83px;" required form="formComprar">
         </td>
         <td class="border-dark fw-bold">${cart[prod].currency}<br>${formatsubt}</td>
         <td class="pt-3 border-bottom-0"><i class='fas fa-times-circle cursor-active' onclick="eliminarprod(${prod})" style='font-size:25px;color:red'></i></td>
@@ -84,7 +109,6 @@ function ShowProductCart(cart) {
             input.addEventListener("input", function () {
                 if (input.value != "") {
                     if (input.value < 1) {
-                        alert("La cantidad tiene que ser almenos 1")
                         input.value = 1;
                     }
                     let textsubtot = Intl.NumberFormat('es-UY').format(cart[input.name].cost * input.value);
@@ -142,7 +166,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 }
             })
-        }else {
+        } else {
             if (Object.keys(cart).length === 0) {
                 CarritoVacio();
             } else {
@@ -156,18 +180,83 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector('input[name="tipodeenvio"]')) {
         document.querySelectorAll('input[name="tipodeenvio"]').forEach((elem) => {
             elem.addEventListener("change", function (event) {
-                costenvio.porcentaje = event.target.value;
-                costenvio.tipo = event.target.id.toString();
+                costenvio = {
+                    tipo: event.target.id.toString(),
+                    porcentaje: event.target.value
+                };
                 actualizarcostos();
             });
         });
     }
-    document.getElementById("mostrarUSD").addEventListener("click",function(){
-        mostrarenuru=false;
+    if (document.querySelector('input[name="formadepago"]')) {
+        document.querySelectorAll('input[name="formadepago"]').forEach((elem) => {
+            elem.addEventListener("change", function (event) {
+                document.getElementById("formapg").value = event.target.value;
+                document.getElementById("num_cuenta").value = "";
+                document.getElementById("num_tarj").value = "";
+                document.getElementById("codseg").value = "";
+                document.getElementById("venc").value = "";
+                if (event.target.value == "Transferencia bancaria") {
+                    document.getElementById("num_cuenta").disabled = false;
+                    document.getElementById("num_tarj").disabled = true;
+                    document.getElementById("codseg").disabled = true;
+                    document.getElementById("venc").disabled = true;
+                } else {
+                    document.getElementById("num_cuenta").disabled = true;
+                    document.getElementById("num_tarj").disabled = false;
+                    document.getElementById("codseg").disabled = false;
+                    document.getElementById("venc").disabled = false;
+                }
+                actualizarfpago();
+            });
+        });
+    }
+    document.getElementById("mostrarUSD").addEventListener("click", function () {
+        mostrarenuru = false;
         ShowProductCart(cart);
     })
-    document.getElementById("mostrarUYU").addEventListener("click",function(){
-        mostrarenuru=true;
+    document.getElementById("mostrarUYU").addEventListener("click", function () {
+        mostrarenuru = true;
         ShowProductCart(cart);
+    })
+    const form = document.getElementById("formComprar");
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        form.classList.add('was-validated');
+        document.getElementById("formapagoModal").classList.add('was-validated');
+        document.getElementById("formapagoModal").classList.add('was-validated');
+        listaprodcart.classList.add('was-validated');
+        actualizarfpago();
+        document.getElementById("num_cuenta").addEventListener("input", actualizarfpago);
+        document.getElementById("num_tarj").addEventListener("input", actualizarfpago);
+        document.getElementById("codseg").addEventListener("input", actualizarfpago);
+        document.getElementById("venc").addEventListener("input", actualizarfpago);
+        if (form.checkValidity()) {
+            const alertexito = document.getElementById('alertexito')
+            const alert = (message, type) => {
+                const wrapper = document.createElement('div')
+                wrapper.innerHTML = [
+                    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+                    `   <div>${message}</div>`,
+                    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+                    '</div>'
+                ].join('')
+
+                alertexito.append(wrapper)
+            }
+            form.reset();
+            document.getElementById("num_cuenta").value = "";
+            document.getElementById("num_tarj").value = "";
+            document.getElementById("codseg").value = "";
+            document.getElementById("venc").value = "";
+            form.classList.remove('was-validated');
+            document.getElementById("formapagoModal").classList.remove('was-validated');
+            listaprodcart.classList.remove('was-validated');
+            costenvio = "No Seleccionado";
+            actualizarcostos();
+            alert('¡Has comprado con exito!', 'success')
+        }
     })
 })
