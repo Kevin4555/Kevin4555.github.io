bce4b9fc83c980e1ea81f4f2c899e1484f3d5a1a
KChino <kevintlapizaga@gmail.com>
2022-11-12 08:56:22 -0300

    Actualizo a bootstrap y perfil
    Actualizo a bootstrap y hago todo lo necesario para implementar el perfil.

--- js/login.js
+++ js/login.js
@@ -1,12 +1,56 @@
+let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+function checks() {
+    if (document.getElementById("maillogin").value == "" || !(validEmail.test(document.getElementById("maillogin").value))) {
+        document.getElementById("maillogin").setCustomValidity("error email");
+    }else{
+        document.getElementById("maillogin").setCustomValidity("");
+    }
+    if (document.getElementById("passwordlogin").value == "") {
+        document.getElementById("passwordlogin").setCustomValidity("error passw");
+    } else {
+        document.getElementById("passwordlogin").setCustomValidity("");
+    } 
+}
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("buttonlogin").addEventListener("click", function () {
-        if (document.getElementById("maillogin").value != "" && document.getElementById("passwordlogin").value != "") {
+    document.getElementById("formlogin").addEventListener("submit", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        checks();
+        if (document.getElementById("formlogin").checkValidity()) {
+            if (localStorage.getItem("users")){
+                let users=JSON.parse(localStorage.getItem("users"));
+                if (!users[document.getElementById("maillogin").value]) {
+                    users[document.getElementById("maillogin").value]={
+                        name:"",
+                        sname:"",
+                        lastname:"",
+                        slastname:"",
+                        telcont:"",
+                        img:""
+                    };
+                    localStorage.setItem("users",JSON.stringify(users));
+                }
+            } else {
+                let users={};
+                users[document.getElementById("maillogin").value]={
+                    name:"",
+                    sname:"",
+                    lastname:"",
+                    slastname:"",
+                    telcont:"",
+                    img:""
+                };
+                localStorage.setItem("users",JSON.stringify(users));
+            };
             localStorage.setItem("usuario_name", document.getElementById("maillogin").value);
             let carrito = {};
             localStorage.setItem("cart_user", JSON.stringify(carrito));
-            window.location.href = "index.html";
+            window.location = "index.html";
+        }else{
+            document.getElementById("formlogin").classList.add(('was-validated'));
+            document.getElementById("maillogin").addEventListener("input",checks);
+            document.getElementById("passwordlogin").addEventListener("input",checks);
         }
-    })
-
-    
+    })   
 });
